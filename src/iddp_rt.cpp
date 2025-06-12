4a37544cb824a442a57b5634b536b64fb3ec0a3d
SHKim-HYU <tjsghd101@naver.com>
2025-06-12 18:19:58 +0900

    update: IDDP

--- src/iddp_rt.cpp
+++ src/iddp_rt.cpp
@@ -43,45 +43,9 @@ static void fail(const char *reason)
 	exit(EXIT_FAILURE);
 }
 
-void rt_client_run(void *arg) {
-    rt_task_set_periodic(NULL, TM_NOW, 100*cycle_ns); // 100ms
-    struct sockaddr_ipc svsaddr, clsaddr;
-    int ret, s, n = 0, len;
-    struct timespec ts;
-    s = __cobalt_socket(AF_RTIPC, SOCK_DGRAM, IPCPROTO_IDDP);
-    if (s < 0)
-            fail("socket");
-    clsaddr.sipc_family = AF_RTIPC;
-    clsaddr.sipc_port = IDDP_CLPORT;
-    ret = __cobalt_bind(s, (struct sockaddr *)&clsaddr, sizeof(clsaddr));
-    if (ret)
-            fail("bind");
-    svsaddr.sipc_family = AF_RTIPC;
-    svsaddr.sipc_port = IDDP_SVPORT;    
-
-    if (ret)
-            fail("bind");    
-    while(1) 
-    {
-        rt_task_wait_period(NULL); //wait for next cycle
-        len = strlen(msg[n]);
-        ret = __cobalt_sendto(s, msg[n], len, 0,
-                        (struct sockaddr *)&svsaddr, sizeof(svsaddr));
-        if (ret < 0) {
-                close(s);
-                fail("sendto");
-        }
-        printf("%s: sent %d bytes, \"%.*s\"\n",
-                __FUNCTION__, ret, ret, msg[n]);
-        n = (n + 1) % (sizeof(msg) / sizeof(msg[0]));
-
-	}
-	return;
-}
-
-
 void rt_server_run(void *arg) {
-    rt_task_set_periodic(NULL, TM_NOW, 100*cycle_ns); // 100ms
+    int server_port = *(int*)arg;
+    rt_task_set_periodic(NULL, TM_NOW, 1*cycle_ns); // 100ms
     struct sockaddr_ipc saddr, claddr;
     socklen_t addrlen;
     char buf[128];
@@ -101,7 +65,7 @@ void rt_server_run(void *arg) {
     if (ret)
             fail("setsockopt");
     saddr.sipc_family = AF_RTIPC;
-    saddr.sipc_port = IDDP_SVPORT;
+    saddr.sipc_port = server_port;
     ret = __cobalt_bind(s, (struct sockaddr *)&saddr, sizeof(saddr));    
     while(1) 
     {
@@ -119,13 +83,71 @@ void rt_server_run(void *arg) {
 	}
 	return;
 }
+
+void rt_client_run(void *arg) {
+    int client_port = *(int*)arg;
+    rt_task_set_periodic(NULL, TM_NOW, 1*cycle_ns); // 100ms
+    struct sockaddr_ipc svsaddr, clsaddr;
+    int ret, s, n = client_port, len, cnt=0;
+    struct timespec ts;
+    char send_buf[256];
+
+    s = __cobalt_socket(AF_RTIPC, SOCK_DGRAM, IPCPROTO_IDDP);
+    if (s < 0)
+            fail("socket");
+    clsaddr.sipc_family = AF_RTIPC;
+    clsaddr.sipc_port = IDDP_CLPORT;
+//     ret = __cobalt_bind(s, (struct sockaddr *)&clsaddr, sizeof(clsaddr));
+//     if (ret)
+//             fail("bind");
+    svsaddr.sipc_family = AF_RTIPC;
+    svsaddr.sipc_port = client_port;    
+
+//     if (ret)
+//             fail("bind");    
+    while(1) 
+    {
+        rt_task_wait_period(NULL); //wait for next cycle
+        snprintf(send_buf, sizeof(send_buf), "%d:%s", cnt, msg[n]);
+
+        len = strlen(send_buf);
+        ret = __cobalt_sendto(s, send_buf, len, 0,
+                        (struct sockaddr *)&svsaddr, sizeof(svsaddr));
+        if (ret >= 0) {
+                printf("%s: sent %d bytes, \"%.*s\"\n",
+                __FUNCTION__, ret, ret, send_buf);
+                n = (n + 1) % (sizeof(msg) / sizeof(msg[0]));
+                cnt++;
+        }
+
+        
+        
+
+	}
+	return;
+}
+
 int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        printf("Usage: %s [1|2]\n", argv[0]);
+        exit(1);
+    }
+
+    int my_port, remote_port;
+    if (atoi(argv[1]) == 1) {
+        my_port = IDDP_SVPORT;
+        remote_port = IDDP_CLPORT;
+    } else {
+        my_port = IDDP_CLPORT;
+        remote_port = IDDP_SVPORT;
+    }
+
     mlockall(MCL_CURRENT|MCL_FUTURE);
 
     rt_task_create(&server_task, "server_task", 0, 99, 0);
-    rt_task_start(&server_task, &rt_server_run, NULL);
+    rt_task_start(&server_task, &rt_server_run, &my_port);
     rt_task_create(&client_task, "client_task", 0, 98, 0);
-    rt_task_start(&client_task, &rt_client_run, NULL);
+    rt_task_start(&client_task, &rt_client_run, &remote_port);
     pause();  // Wait for Ctrl+C
 
     rt_task_delete(&server_task);
@@ -133,4 +155,3 @@ int main(int argc, char* argv[]) {
 
     return 0;
 }
-
